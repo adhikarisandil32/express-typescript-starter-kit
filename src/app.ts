5b2ef1e3c53b4a9880c754ce44731eee61496f00
Sandil Adhikari <adhikarisandil32@gmail.com>
2025-08-29 02:16:50 +0545

    prepare API_DOCS_ENDPOINT and API_PREFIX and assign each to root route

--- src/app.ts
+++ src/app.ts
@@ -3,56 +3,31 @@ import morgan from "morgan";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocs from "@src/swagger-doc/swagger.json" with { type: "json" }; // assertion required for ecmascript json imports
 import { config } from "@src/config/config.js";
+import { rootRoute } from "./routes/root.route";
 
 // Visit https://editor.swagger.io/ and/or https://swagger.io/docs/specification/v3_0/api-host-and-base-path/ for more details on api docs practice and learning
 
+// api docs endpoint
+const API_DOCS_ENDPOINT = !config.appConfig.API_DOCS_ENDPOINT
+  ? "/"
+  : config.appConfig.API_DOCS_ENDPOINT.startsWith("/")
+    ? config.appConfig.API_DOCS_ENDPOINT
+    : `/${config.appConfig.API_DOCS_ENDPOINT}`;
+
+//api prefix
+const API_PREFIX = !config.appConfig.API_PREFIX
+  ? "/"
+  : config.appConfig.API_PREFIX.startsWith("/")
+    ? config.appConfig.API_PREFIX
+    : `/${config.appConfig.API_PREFIX}`;
+
 const app = express();
-const API_PREFIX = config.API_PREFIX?.startsWith("/")
-  ? config.API_PREFIX
-  : `/${config.API_PREFIX}`;
 
 app.use(morgan("tiny"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(
-  `/${config.API_DOCS_ENDPOINT}`,
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerDocs),
-);
+app.use(API_DOCS_ENDPOINT, swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-app.get(`${API_PREFIX}`, (_, res) => {
-  res.json({ Test: "success" });
-  return;
-});
-
-app.post(`${API_PREFIX}/products`, (req, res) => {
-  res.json({
-    status: 200,
-    message: "Operation Successful",
-    data: {
-      id: 1,
-      name: req.body?.name,
-      description: req.body?.description,
-    },
-  });
-
-  return;
-});
-
-app.patch(`${API_PREFIX}/products/:id`, (req, res) => {
-  const productId = req.params.id;
-
-  res.json({
-    status: 200,
-    message: "Operation Successful",
-    data: {
-      id: productId,
-      name: req.body?.name,
-      description: req.body?.description,
-    },
-  });
-
-  return;
-});
+app.use(API_PREFIX, rootRoute);
 
 export { app };
